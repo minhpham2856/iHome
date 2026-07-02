b4bdfbf723f707e53fbcb30c2acedc32c2cbd0ea
minhpham2856 <minhphamnhat2852006@gmail.com>
2026-07-02 15:00:54 +0700

    Update authentication logic in BLL and DAL

---
+++ iHome.BLL/Common/InputFormatter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iHome.BLL.Common
+{
+	public static class InputFormatter
+	{
+		// Format full name to uppercase and trim whitespace
+		public static string FormatFullName(string fullname)
+		{
+			if (string.IsNullOrWhiteSpace(fullname)) return string.Empty;
+			return fullname.Trim().ToUpper();
+		}
+	}
+}
---
+++ iHome.BLL/Common/InputValidator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iHome.BLL.Common
+{
+	public static class InputValidator
+	{
+		public static bool IsValidUsername(string username)
+		{
+			if (string.IsNullOrWhiteSpace(username)) return false;
+			if (username.Length < 3 || username.Length > 20) return false;
+			return username.All(c => char.IsLetterOrDigit(c) || c == '_' || c == '.');
+		}
+
+		public static bool IsValidPassword(string password)
+		{
+			if (string.IsNullOrWhiteSpace(password)) return false;
+			if (password.Length < 8 || password.Length > 20) return false;
+			return password.Any(char.IsUpper) && password.Any(char.IsLower) && password.Any(char.IsDigit) && password.Any(c => !char.IsLetterOrDigit(c));
+		}
+	}
+}
